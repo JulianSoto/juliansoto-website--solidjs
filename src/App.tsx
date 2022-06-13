2782e558f2e2b69613884a1912776f56fb7eaa51
Julian Soto <juliansotoponce@gmail.com>
2022-06-12 22:35:35 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1>Julian Soto</h1>
+      <span>Web Developer</span>
+    </div>
+  );
+};
+
+export default App;
